fbeb0a23ac34a75112e414049d38fb7af62a201d
Roger <sent2roger@gmail.com>
2012-03-08 00:17:20 +0600

    buttons added

--- gen/com/developer4droid/weight_test/R.java
+++ gen/com/developer4droid/weight_test/R.java
@@ -13,6 +13,13 @@ public final class R {
     public static final class color {
         public static final int any_button_stroke=0x7f040002;
         public static final int blue=0x7f040022;
+        /**     <color name="blue_button_inside_shadow">#7F000000</color>
+         */
+        public static final int blue_button_inside_shadow=0x7f04003b;
+        public static final int blue_button_reversed_inside_stroke=0x7f040039;
+        /** <color name="blue_emboss_low_right">#FF63a62d</color>
+         */
+        public static final int blue_emboss_top_left=0x7f040037;
         public static final int dark_blue=0x7f040019;
         public static final int dark_light=0x7f040017;
         public static final int dark_orange=0x7f040015;
@@ -33,6 +40,7 @@ public final class R {
         public static final int f_light_blue_1=0x7f04001e;
         public static final int f_light_grey=0x7f04001a;
         public static final int f_stroke=0x7f040020;
+        public static final int game_board_back=0x7f040034;
         /** <color name="dark_orange">#FFa94a14</color>
          */
         public static final int glow_border=0x7f040024;
@@ -78,51 +86,113 @@ public final class R {
         /** Buttons
          */
         public static final int stroke_shadow=0x7f040001;
+        /** Toggle buttons
+         */
+        public static final int toggle_button_1=0x7f040032;
+        /** <color name="toggle_button_1">#FF3d3d3d</color>
+         */
+        public static final int toggle_button_2=0x7f040033;
+        /** <color name="toggle_button_inside_stroke">#FF191919</color>
+         */
+        public static final int toggle_button_inside_stroke=0x7f040038;
+        public static final int toggle_button_outer_shadow=0x7f04003a;
+        public static final int toggle_button_p_1=0x7f040035;
+        public static final int toggle_button_p_2=0x7f040036;
         public static final int top_bar_1=0x7f04002c;
         public static final int top_bar_2=0x7f04002d;
         public static final int top_bar_3=0x7f04002e;
         public static final int top_bar_stroke_1=0x7f04002a;
         public static final int top_bar_stroke_2=0x7f04002b;
     }
+    public static final class dimen {
+        public static final int air_switch_button_height=0x7f07000f;
+        public static final int blue_bar_button_height=0x7f070005;
+        public static final int blue_button_big_height=0x7f070003;
+        public static final int blue_button_height=0x7f070002;
+        public static final int blue_button_min_width=0x7f070004;
+        public static final int blue_button_padding_def_bottom=0x7f070009;
+        public static final int blue_button_padding_def_left=0x7f070007;
+        public static final int blue_button_padding_def_right=0x7f070008;
+        public static final int blue_button_padding_def_top=0x7f070006;
+        public static final int default_stroke_width=0x7f07000e;
+        public static final int toggle_button_padding_bottom=0x7f07000d;
+        public static final int toggle_button_padding_left=0x7f07000a;
+        public static final int toggle_button_padding_right=0x7f07000b;
+        public static final int toggle_button_padding_top=0x7f07000c;
+        public static final int toggle_button_radius=0x7f070000;
+        public static final int toggle_button_stroke_width=0x7f070001;
+    }
     public static final class drawable {
-        public static final int captured_bb=0x7f020000;
-        public static final int captured_bk=0x7f020001;
-        public static final int captured_bn=0x7f020002;
-        public static final int captured_bp=0x7f020003;
-        public static final int captured_bq=0x7f020004;
-        public static final int captured_br=0x7f020005;
-        public static final int captured_wb=0x7f020006;
-        public static final int captured_wk=0x7f020007;
-        public static final int captured_wn=0x7f020008;
-        public static final int captured_wp=0x7f020009;
-        public static final int captured_wq=0x7f02000a;
-        public static final int captured_wr=0x7f02000b;
-        public static final int ic_action_done=0x7f02000c;
-        public static final int shifted_image=0x7f02000d;
-        public static final int tap_1=0x7f02000e;
-        public static final int tap_2=0x7f02000f;
-        public static final int tap_3=0x7f020010;
-        public static final int tap_4=0x7f020011;
-        public static final int tap_5=0x7f020012;
-        public static final int tap_6=0x7f020013;
-        public static final int wp_six=0x7f020014;
+        public static final int button_emboss_left_default=0x7f020000;
+        public static final int button_emboss_left_pressed=0x7f020001;
+        public static final int button_emboss_left_selector=0x7f020002;
+        public static final int button_emboss_mid_default=0x7f020003;
+        public static final int button_emboss_mid_pressed=0x7f020004;
+        public static final int button_emboss_mid_selector=0x7f020005;
+        public static final int button_emboss_right_default=0x7f020006;
+        public static final int button_emboss_right_pressed=0x7f020007;
+        public static final int button_emboss_right_selector=0x7f020008;
+        public static final int captured_bb=0x7f020009;
+        public static final int captured_bk=0x7f02000a;
+        public static final int captured_bn=0x7f02000b;
+        public static final int captured_bp=0x7f02000c;
+        public static final int captured_bq=0x7f02000d;
+        public static final int captured_br=0x7f02000e;
+        public static final int captured_wb=0x7f02000f;
+        public static final int captured_wk=0x7f020010;
+        public static final int captured_wn=0x7f020011;
+        public static final int captured_wp=0x7f020012;
+        public static final int captured_wq=0x7f020013;
+        public static final int captured_wr=0x7f020014;
+        public static final int ic_action_done=0x7f020015;
+        public static final int ic_add=0x7f020016;
+        public static final int ic_analysis=0x7f020017;
+        public static final int ic_back=0x7f020018;
+        public static final int ic_chat=0x7f020019;
+        public static final int ic_chat_nm=0x7f02001a;
+        public static final int ic_fastforward=0x7f02001b;
+        public static final int ic_flip=0x7f02001c;
+        public static final int ic_forward=0x7f02001d;
+        public static final int ic_help=0x7f02001e;
+        public static final int ic_hint=0x7f02001f;
+        public static final int ic_options=0x7f020020;
+        public static final int ic_search=0x7f020021;
+        public static final int ic_share=0x7f020022;
+        public static final int shifted_image=0x7f020023;
+        public static final int tap_1=0x7f020024;
+        public static final int tap_2=0x7f020025;
+        public static final int tap_3=0x7f020026;
+        public static final int tap_4=0x7f020027;
+        public static final int tap_5=0x7f020028;
+        public static final int tap_6=0x7f020029;
+        public static final int wp_six=0x7f02002a;
     }
     public static final class id {
-        public static final int addBtn=0x7f070006;
-        public static final int blackCapturedPieces=0x7f070003;
-        public static final int changeView=0x7f07000a;
-        public static final int controlButtonsPanel=0x7f070000;
-        public static final int fastForwardBtn=0x7f070001;
-        public static final int movesListView=0x7f070004;
-        public static final int removeBtn=0x7f070007;
-        public static final int seekView=0x7f070009;
-        public static final int weightView=0x7f070008;
-        public static final int weightView2=0x7f070005;
-        public static final int whiteCapturedPieces=0x7f070002;
+        public static final int addBtn=0x7f08000d;
+        public static final int analysisBtn=0x7f080005;
+        public static final int backBtn=0x7f080007;
+        public static final int blackCapturedPieces=0x7f08000a;
+        public static final int changeView=0x7f080011;
+        public static final int chatBtn=0x7f080006;
+        public static final int controlButtonsPanel=0x7f080001;
+        public static final int fastForwardBtn=0x7f080002;
+        public static final int flipBtn=0x7f080004;
+        public static final int forwardBtn=0x7f080008;
+        public static final int gamePanel=0x7f080000;
+        public static final int movesListView=0x7f08000b;
+        public static final int optionsBtn=0x7f080003;
+        public static final int removeBtn=0x7f08000e;
+        public static final int seekView=0x7f080010;
+        public static final int weightView=0x7f08000f;
+        public static final int weightView2=0x7f08000c;
+        public static final int whiteCapturedPieces=0x7f080009;
     }
     public static final class layout {
         public static final int game_panel=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int game_panel2=0x7f030001;
+        public static final int game_panel_list_item=0x7f030002;
+        public static final int game_panel_weights=0x7f030003;
+        public static final int main=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
@@ -137,8 +207,15 @@ public final class R {
          */
         public static final int DefaultText=0x7f060003;
         public static final int GamePanelButton=0x7f060006;
+        public static final int button_blue=0x7f060008;
+        public static final int button_blue_left=0x7f06000c;
+        public static final int button_blue_mid=0x7f06000e;
+        public static final int button_blue_right=0x7f06000d;
         public static final int custom_edit_field=0x7f060000;
+        public static final int image_button_blue=0x7f06000a;
         public static final int spinnerStyleDropDown=0x7f060004;
         public static final int spinnerText=0x7f060005;
+        public static final int tap_button=0x7f060009;
+        public static final int toggle_button=0x7f06000b;
     }
 }
